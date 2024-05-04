6aea948e734f400f0718f26375756284a86789c7
vinay <vinaykalva712@gmail.com>
2024-05-04 22:03:38 +0000

    commit message

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,120 @@
+import {Component} from 'react'
+import ThunmNail from '../ThumbnailItem'
+import './index.css'
+
+const imagesList = [
+  {
+    id: 0,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-pond-thumbnail-img.png',
+    imageAltText: 'nature mountain with pond',
+    thumbnailAltText: 'nature mountain with pond thumbnail',
+  },
+  {
+    id: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-sunset-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-sunset-thumbnail-img.png',
+    imageAltText: 'nature sunset',
+    thumbnailAltText: 'nature sunset thumbnail',
+  },
+  {
+    id: 2,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-ocean-thumbnail-img.png',
+    imageAltText: 'nature mountain with ocean',
+    thumbnailAltText: 'nature mountain with ocean thumbnail',
+  },
+  {
+    id: 3,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-mountain-with-forest-thumbnail-img.png',
+    imageAltText: 'nature mountain with forest',
+    thumbnailAltText: 'nature mountain with forest thumbnail',
+  },
+  {
+    id: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-leaves-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-leaves-thumbnail-img.png',
+    imageAltText: 'nature leaves',
+    thumbnailAltText: 'nature leaves thumbnail',
+  },
+  {
+    id: 5,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-tree-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-tree-thumbnail-img.png',
+    imageAltText: 'nature tree',
+    thumbnailAltText: 'nature tree thumbnail',
+  },
+  {
+    id: 6,
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-waterfall-thumbnail-img.png',
+    imageAltText: 'nature waterfall',
+    thumbnailAltText: 'nature waterfall thumbnail',
+  },
+  {
+    id: 7,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/nature-river-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/nature-river-thumbnail-img.png',
+    imageAltText: 'nature river',
+    thumbnailAltText: 'nature river thumbnail',
+  },
+]
+
+// Write your code here.
+
+class Gallery extends Component {
+  state = {
+    tabId: imagesList[0].id,
+    imageUrl: imagesList[0].imageUrl,
+    imageAltText: imagesList[0].imageAltText,
+  }
+
+  onChangeImg = id => {
+    const reqImg = imagesList.find(eachImg => eachImg.id === id)
+    this.setState({
+      imageUrl: reqImg.imageUrl,
+      imageAltText: reqImg.imageAltText,
+      tabId: reqImg.id,
+    })
+  }
+
+  render() {
+    const {tabId, imageUrl, imageAltText} = this.state
+    return (
+      <div className="Photography-cont">
+        <div>
+          <img src={imageUrl} alt={imageAltText} className="main-image" />
+          <div className="desc-cont">
+            <h1 className="main-heading">Nature Photography</h1>
+            <p className="para">Nature Photography by Rahul</p>
+            <ul className="thumnails-cont">
+              {imagesList.map(eachImgDetails => (
+                <ThunmNail
+                  imageDetails={eachImgDetails}
+                  key={eachImgDetails.id}
+                  onChangeImg={this.onChangeImg}
+                  isActive={tabId === eachImgDetails.id}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Gallery
